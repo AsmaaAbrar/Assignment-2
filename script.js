36296e9838904238f7dda493f87c9594417509ab
AsmaaAbrar <51248664+AsmaaAbrar@users.noreply.github.com>
2020-04-16 19:10:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var index = 1;
+function changeImgFun(){
+  var images = [ 'img1.jpg', 'img2.jpg','img3.jpg','img4.jpg' ];
+  var img = document.getElementById("img");
+  img.src = images[index];
+  index++;
+  if( index==images.length ){
+    index = 0;
+  }
+}
+
+function displayAlertFun(){
+  var correctAnswer = document.getElementById('correctAnswer');
+  var wrongAnswer1  = document.getElementById('wrongAnswer1');
+  var wrongAnswer3  = document.getElementById('wrongAnswer2');
+  var wrongAnswer3  = document.getElementById('wrongAnswer3');
+
+  if(correctAnswer.checked){
+    alert("Correct Answerr");
+  }
+  else if (wrongAnswer1.checked || wrongAnswer2.checked || wrongAnswer3.checked) {
+    alert("Wrong Answerr");
+  }
+  else {
+    alert("Choose answer please");
+  }
+}
+
+// // another answer using querySelector
+// function displayAlertFun(){
+//   var correctAnswer = 'option1'
+//   var answer = document.querySelector('input[name="choice"]:checked').value;
+//   (answer === correctAnswer)
+//     ? alert("Correct Answerr")
+//     : alert("Wrong Answerr");
+// }
